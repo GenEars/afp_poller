793ce9275e53e0425aefc30d9a79355c0b69e913
HERONSURFACE\lione <lionel.tailhardat@orange.fr>
2020-11-11 12:56:00 +0100

    news polling as functions'

--- afp_poller.py
+++ afp_poller.py
@@ -8,6 +8,8 @@ Created on Mon Nov  9 21:27:31 2020
 import requests
 import logging
 import random
+import platform
+
 
 #from tkinter import *  # https://python.doctor/page-tkinter-interface-graphique-python-tutoriel
 import tkinter as tk
@@ -18,61 +20,74 @@ import tkinter as tk
 loggingFormatString = '%(asctime)s:%(levelname)s:%(threadName)s:%(funcName)s:%(message)s'
 logging.basicConfig(format=loggingFormatString, level=logging.DEBUG)
 
-# Initialisation des URL
-url_auth = "https://api.afp.com/oauth/token?grant_type=anonymous"
-url_search = "https://api.afp.com:443/v1/api/search?wt=xml&access_token="
 
 # =============================================================================
 # Démarrage  du traitement
 
 logging.info('INIT')
 
-# =============================================================================
-# Récupération du jeton d'authentification
-r_auth = requests.get(url_auth)
-
-logging.info("AUTH:status_code=%s", r_auth.status_code)
+logging.info("INIT:platform.system=%s", platform.system())
 
 # =============================================================================
-# Récupération des news
 
-max_rows = 50
+def ws_auth_token(
+        ws_url="https://api.afp.com/oauth/token?grant_type=anonymous"):
+    """ Récupération du jeton d'authentification """  
 
-payload = """<?xml version="1.0"?>
-<Parameters>
-  <dateRange>
-    <from>now-3d</from>
-    <to>now</to>
-  </dateRange>
-  <lang>fr</lang>
-  <maxRows>50</maxRows>
-</Parameters>
-"""
-# TODO : include max_rows dans payload
-
-headers = { 'Content-Type': 'application/xml'}
-
-url_search_full = url_search + r_auth.json()['access_token']
-r_search = requests.post(url_search_full, data=payload, headers=headers)
-
-logging.info("SEARCH:status_code=%s", r_search.status_code)
-
-# =============================================================================
-# Traitement des news
-
-r_search_response = r_search.json()['response']
-
-for news in r_search_response['docs']:
+    r_auth = requests.get(ws_url)
+    logging.info("AUTH:status_code=%s", r_auth.status_code)
     
-    logging.info("NEWS:headline=%s", news['headline'])
+    r_auth_token = r_auth.json()['access_token']
+    logging.debug("AUTH:access_token=%s", r_auth_token)
+    
+    return r_auth_token
+
+
+def ws_get_news(
+        ws_url="https://api.afp.com:443/v1/api/search?wt=xml&access_token=",
+        access_token=None,
+        max_rows=50):
+    """ Récupération des news """
+
+    # Request parameters
+    payload = """<?xml version="1.0"?>
+    <Parameters>
+      <dateRange>
+        <from>now-3d</from>
+        <to>now</to>
+      </dateRange>
+      <lang>fr</lang>
+      <maxRows>{max_rows}</maxRows>
+    </Parameters>
+    """.format(max_rows=max_rows)
+    
+    headers = { 'Content-Type': 'application/xml'}
+    url_search_full = ws_url + access_token
+    
+    # Request
+    r_search = requests.post(url_search_full, data=payload, headers=headers)
+    logging.info("SEARCH:status_code=%s", r_search.status_code)
+
+    r_search_response = r_search.json()['response']
+    logging.debug("SEARCH:r_search_response=%s", r_search_response)
+    
+    return r_search_response
+
+
+def random_news_index(data):
+    """ Get a valid random index over retrieved news """
+    return random.randint(0, len(data['docs']) - 1)
 
 
 # =============================================================================
-# Marquee
-# Source: https://stackoverflow.com/questions/47224061/how-to-make-marquee-on-tkinter-in-label
+# GUI definitions
 
 
 class Marquee(tk.Canvas):
+    """
+        Marquee class
+        Source: https://stackoverflow.com/questions/47224061/how-to-make-marquee-on-tkinter-in-label
+    """
     def __init__(self, parent, text, margin=2, borderwidth=0,
                  relief='flat', fps=30,
                  bg='black', fg='white',
@@ -124,11 +139,8 @@ class Marquee(tk.Canvas):
         self.after_id = self.after(int(1000/self.fps), self.animate)
 
 
-def random_news_index():
-    return random.randint(0, len(r_search_response['docs']) - 1)
-
-
 def make_marquee(parent_win, marquee_text, font_size, marquee_pady, index):
+    """ Create a Marquee canvas on parent_win """
     marquee = Marquee(
             parent_win,
             text=marquee_text,
@@ -138,19 +150,45 @@ def make_marquee(parent_win, marquee_text, font_size, marquee_pady, index):
     marquee.pack(side="top", fill="x", pady=marquee_pady)
 
 
+def delete_marquee(parent_win, index):
+    """ Delete a Marquee canvas on parent_win """
+    pass
+
+
 def clavier(event):
+    """ GUI's keyboard event handler """
     touche = event.keysym
     logging.debug("EVENT:clavier:touche=%s", touche)
 
     if touche == "Escape":
-        pass
+        root.destroy()
     elif touche == "space":
         pass
         
+def remove_title_bar(parent_win):
+    """ Remove GUI's title bar """
+
+    if platform.system() == "Linux":
+        # Tk 8.5 or above, Linux system
+        root.wm_attributes('-type', 'splash')
+    
+    # TODO: remove title bar for Windows OS
+    
 # =============================================================================
 
+# News polling
+news_data = ws_get_news(access_token=ws_auth_token())
+
+for news in news_data['docs']:
+    
+    logging.debug("NEWS:headline=%s", news['headline'])
+
+# GUI init
 root = tk.Tk()
 root.configure(bg='black')
+root.title(__file__)
+root.state("zoomed")
+remove_title_bar(root)
 root.bind("<Key>", clavier)
 
 base_font_size=10
@@ -159,7 +197,7 @@ base_paddy=10
 for index in range(0, 10):
     make_marquee(
             root,
-            r_search_response['docs'][random_news_index()]['headline'],
+            news_data['docs'][random_news_index(news_data)]['headline'],
             font_size = base_font_size + random.randint(-base_font_size + 5, 40),
             marquee_pady = base_paddy + random.randint(-base_paddy + 1, base_paddy + 10),
             index=index)
@@ -168,10 +206,10 @@ root.mainloop()
 
 # TODO: update des news
 # TODO: update des marquee
-# TODO: dimensionnement max
-# TODO: disparition de la tête de fenêtre
 # TODO: appel à distance
-# TODO: gestion de la fermeture
+# TODO: argparse avec log level
+# TODO: fps dans argparse
+# TODO: logging dans syslog
 
 # =============================================================================
 
